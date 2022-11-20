70b861995bfbcea1f1ca8ff5a2118ef070d3ca30
jgmejiaco <jgmejiaco@gmail.com>
2022-11-20 11:32:14 -0500

    Inicio de Proyecto Avaluamos, commit inicial

---
+++ database/migrations/2022_11_14_162910_create_table_estado.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableEstado extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('estado', function (Blueprint $table) {
+            $table->increments('id_estado');
+            $table->string('descripcion_estado');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('estado');
+    }
+}
