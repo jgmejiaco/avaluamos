dcc85fe8efcd256bbdf7a0e05d969413e440b26e
jgmejiaco <jgmejiaco@gmail.com>
2023-11-06 08:20:19 -0500

    inclusion jquery3.5.1.min

--- avaluamos_web/resources/views/layouts/app.blade.php
+++ avaluamos_web/resources/views/layouts/app.blade.php
@@ -29,7 +29,7 @@
 
         <!--  Js -->
         <script src="{{asset('js/modernizr.custom.js')}}"></script>
-        <script src="{{asset('js/jquery-3.6.0.min.js')}}"></script>
+        <script src="{{asset('js/jquery-3.5.1.min.js')}}"></script>
 
         {{-- ========================================= --}}
 
