d4c48adad8867a731fc77686a55fba56dc719ed9
Juangui <jgmejiaco@gmail.com>
2024-05-01 11:45:41 -0500

    migración a lravel 9

--- avaluamos_web/app/Providers/AppServiceProvider.php
+++ avaluamos_web/app/Providers/AppServiceProvider.php
@@ -3,10 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\View;
-use Illuminate\Support\Facades\DB;
-use Exception;
-use App\Models\Rol;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -27,31 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        View::composer('layouts.topbar', function ($view) {
-            try {
-                $usuLogueado = session('id_usuario');
-
-                $usuRolPermiso = DB::table('rol')
-                                    ->leftJoin('usuarios', 'usuarios.id_rol', '=', 'rol.id_rol')
-                                    ->select(
-                                        'id_usuario',
-                                        'rol.id_rol',
-                                        'mod_usuario',
-                                        'mod_clientes',
-                                        'mod_calendario',
-                                        'mod_visitas',
-                                        'mod_avaluo',
-                                        'mod_informes'
-                                    )
-                                    ->where('id_usuario', $usuLogueado)
-                                    ->first();
-
-                $view->with('permiso', $usuRolPermiso);
-
-            } catch (Exception $e) {
-                alert()->error('Error', 'An error has occurred, try again, if the problem persists contact support.');
-                return back();
-            }
-        });
+        //
     }
 }
